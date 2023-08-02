9d82edc202b571353097c8461df07215c8ea9221
arunjo5 <arunjo528@gmail.com>
2023-08-02 18:30:40 -0400

    Added CSV Chatbot functionality

--- app.py
+++ app.py
@@ -121,7 +121,81 @@ if selected_page == "BizMatch":
         main()
 
 elif selected_page == "BizBot":
-                
+    AI21_API_KEY = st.secrets["AI21_API_KEY"]
+    st.subheader("Chat about a CSV Document ")
+    uploaded_file = st.file_uploader('', type='csv', accept_multiple_files=False)
+
+    if uploaded_file:
+        
+        some_bytes = uploaded_file.getvalue()
+        with open("my_file.csv", "wb") as binary_file:
+            binary_file.write(some_bytes)
+            base = Path.cwd()
+            PATH_TO_FILE = f"{base}/my_file.csv"
+
+        markdown = MDTable(PATH_TO_FILE)
+        markdown_string_table = markdown.get_table()
+        with st.expander("Input Table", expanded=False):
+            st.write(markdown_string_table)
+        markdown.save_table('out.csv')
+
+        def get_answer(user_input):
+            response = requests.post("https://api.ai21.com/studio/v1/experimental/j1-grande-instruct/complete",
+            headers={"Authorization": "Bearer " + AI21_API_KEY},
+            json={
+            "prompt": markdown_string_table + "\nQ: " + user_input + "\nA:",
+            "numResults": 1,
+            "maxTokens": 200,
+            "temperature": 0,
+            "topKReturn": 0,
+            "topP":1,
+            "countPenalty": {
+                    "scale": 0,
+                    "applyToNumbers": False,
+                    "applyToPunctuations": False,
+                    "applyToStopwords": False,
+                    "applyToWhitespaces": False,
+                    "applyToEmojis": False
+            },
+            "frequencyPenalty": {
+                    "scale": 0,
+                    "applyToNumbers": False,
+                    "applyToPunctuations": False,
+                    "applyToStopwords": False,
+                    "applyToWhitespaces": False,
+                    "applyToEmojis": False
+            },
+            "presencePenalty": {
+                    "scale": 0,
+                    "applyToNumbers": False,
+                    "applyToPunctuations": False,
+                    "applyToStopwords": False,
+                    "applyToWhitespaces": False,
+                    "applyToEmojis": False
+            },
+            "stopSequences":["↵↵"]
+            }
+        )
+
+            return response.json()
+        
+        def get_text():
+            input_text = st.text_input("You: ","")
+            return input_text 
+
+        user_inputs = get_text()
+
+        if user_inputs:
+            res = get_answer(user_inputs)
+            # st.write(res)
+            try:
+                answer = res["completions"][0]["data"]["text"]
+                message(user_inputs, is_user=True)
+                message(answer)
+            except:
+                st.error(res['detail'])
+
+           
     openai.api_key = st.secrets["OPENAI_KEY"]
     st.subheader("Or ask any General Questions")
     query = st.text_input("Have any business questions?")
